f857e78234add2329bd2069027d6aaacb6573403
soojeong55 <ghkdtnwjd55@gmail.com>
2020-05-08 03:31:53 +0900

    polls

--- polls/models.py
+++ polls/models.py
@@ -17,7 +17,7 @@ class Question(models.Model):
 
     was_published_recently.admin_order_field = 'pub_date'  # 추가
     was_published_recently.boolean = True  # 추가
-    was_published_recently.short_description = '최근 게시?'  # 추가
+    was_published_recently.short_description = '최근 게시?'  # 추가.
 
 
 class Choice(models.Model):
